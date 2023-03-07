cafddbc34494adbad91483589493696a5f136ea5
Jane <evgenia.kochukova@gmail.com>
2023-03-07 16:08:57 -0300

    Reformation code

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -2,10 +2,15 @@ package ru.netology;
 
 public class Radio {
 
-    public int currentStation;
+    private int currentStation;
+    private int currentVolume;
 
     public int getCurrentStation() {
         return currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
     }
 
     public void setCurrentStation(int newCurrentStation) {
@@ -18,26 +23,6 @@ public class Radio {
         currentStation = newCurrentStation;
     }
 
-    public void setNextStation() {
-        if (currentStation < 9) {
-            currentStation = currentStation + 1;
-
-        } else currentStation = 0;
-
-    }
-
-    public void setPreviewStation() {
-        if (currentStation > 0) {
-            currentStation = currentStation - 1;
-        } else currentStation = 9;
-    }
-
-    public int currentVolume;
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
     public void setCurrentVolume(int newCurrentVolume) {
         if (newCurrentVolume < 0) {
             newCurrentVolume = 0;
@@ -48,16 +33,36 @@ public class Radio {
         currentVolume = newCurrentVolume;
     }
 
+    public void setNextStation() {
+        if (currentStation < 9) {
+            currentStation = currentStation + 1;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void setPreviewStation() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        } else {
+            currentStation = 9;
+        }
+    }
+
     public void increaseVolume() {
         if (currentVolume < 10) {
             currentVolume = currentVolume + 1;
-        } else currentVolume = 10;
+        } else {
+            currentVolume = 10;
+        }
     }
 
     public void reduceVolume() {
         if (currentVolume > 0) {
             currentVolume = currentVolume - 1;
-        } else currentVolume = 0;
+        } else {
+            currentVolume = 0;
+        }
     }
 }
 
